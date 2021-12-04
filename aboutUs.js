3ece729c8f5202224ae9dc9309b2f854c901e6b2
Rahul <rahulgoyal01052003@gmail.com>
2021-12-04 19:38:31 +0530

    Pre final commit

---
+++ aboutUs.js
@@ -0,0 +1,58 @@
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+const removeBg1 = document.querySelector("#pills-ADMINISTRATION-tab");
+const removeBg2 = document.querySelector("#pills-2020-2021-tab");
+var i = 1;
+var id_2T ;
+if(vw<="760"){
+    removeBg1.classList.remove("active");
+    removeBg2.classList.remove("active");
+}
+team = (id) => {
+    if(vw<="760"){
+        const a = document.getElementById(id);
+        var id_1 = a.id;
+        const tab = document.querySelector(".tab-content");
+        if (i == 1) {
+            a.insertAdjacentElement("afterend", tab);
+            tab.style.display = "block";
+            id_2T = id_1;
+        } else if (i == 2 && id_1==id_2T) {
+            tab.style.display = "none";
+            a.classList.remove("active");
+
+        i=0;
+    }else if (i==2&& id_1!=id_2T){
+        a.insertAdjacentElement("afterend", tab);
+        id_2T=id_1;
+        i=1;
+    }else{
+        console.log("Show error");
+  }
+  i++;
+};
+}
+var j = 1;
+var id_2A ;
+alum = (id) => {
+if(vw<="760"){
+    const a = document.getElementById(id);
+    var id_1 = a.id
+    const tab = document.querySelector(".tab-content-alum");
+    if (j == 1) {
+        a.insertAdjacentElement("afterend", tab);
+        tab.style.display = "block";
+        id_2A = id_1;
+    } else if (j == 2 && id_1==id_2A) {
+        tab.style.display = "none";
+        a.classList.remove("active");
+        j=0;
+    }else if (i==2&& id_1!=id_2A){
+        a.insertAdjacentElement("afterend", tab);
+        id_2A=id_1;
+        j=1;
+    }else{
+        console.log("Show error");
+  }
+  j++;
+};
+}
