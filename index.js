0b3ac1f6f485f9fd1b04af9b04af44835b755f8c
RAHUL GOYAL <83687972+Rahul-go1503@users.noreply.github.com>
2022-02-07 16:30:22 +0530

    navbar ~~testing

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ toggle.onclick = (e) => {
   trigg.classList.toggle("is-active");
   box.classList.toggle("collapsed");
 };
+
+// Menu shrink Js
+window.onscroll = ()=> {
+  if (document.body.scrollTop > 93 || document.documentElement.scrollTop > 80) {
+    document.getElementById("ncc-heading").style.display = "none";
+    document.getElementById("NCC-Logo").style.height = "70px";
+    document.getElementById("IITR-Logo").style.alignSelf = "center";
+    document.getElementById("navbar_links").style.height = "100px";
+  } else {
+    document.getElementById("navbar_links").style.height = "150px";
+    document.getElementById("ncc-heading").style.display = "block";
+  }
+}
 // Footer JS
 const footer = (id, icon) => {
   var element = document.getElementById(id);
